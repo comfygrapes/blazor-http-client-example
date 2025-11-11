9a32e3fa2aeae42edc1e74269a932c56c5bedd26
Vince Russell Recentes <comfygrapesreal@gmail.com>
2025-11-11 09:26:01 +0800

    chore: change readability of httpclient registration

--- Blazor.HttpClientExample.App/MauiProgram.cs
+++ Blazor.HttpClientExample.App/MauiProgram.cs
@@ -17,7 +17,11 @@ namespace Blazor.HttpClientExample.App
 
             builder.Services.AddMauiBlazorWebView();
 
-            builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://jsonplaceholder.typicode.com") });
+            builder.Services.AddSingleton(sp => 
+                new HttpClient { 
+                    BaseAddress = new Uri("https://jsonplaceholder.typicode.com") 
+                }
+            );
 
             builder.Services.AddSingleton<IHomePresenter, HomePresenter>();
 
